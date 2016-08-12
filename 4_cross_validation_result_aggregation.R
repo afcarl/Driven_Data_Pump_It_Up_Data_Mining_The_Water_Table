036d4f2fe87720d7a9e7f800905fe17abaa1a79f
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2016-08-12 16:33:26 +0200

    Update 4_cross_validation_result_aggregation.R

--- 4_cross_validation_result_aggregation.R
+++ 4_cross_validation_result_aggregation.R
@@ -1,4 +1,3 @@
-
 #####
 #
 # The cross validation results are in different comma separated values files.
@@ -7,7 +6,7 @@
 #####
 
 cross_validation <- list.files("./cross_validation/")
-cross_valitation_aggregated_results <- data.frame(matrix(0,length(cross_validation),6))
+cross_valitation_aggregated_results <- data.frame(matrix(0, length(cross_validation), 6))
 colnames(cross_valitation_aggregated_results) <- c("depth",
                                                    "eta",
                                                    "subsample",
@@ -24,14 +23,14 @@ colnames(cross_valitation_aggregated_results) <- c("depth",
 i <- 0
 for (c in cross_validation){
   i <- i + 1
-  cross_validation_results <- read.csv(paste0("./cross_validation/",c),stringsAsFactors = FALSE)
-  depth <- strsplit(c,"_")[[1]][3]
-  eta <- strsplit(c,"_")[[1]][5]
-  subsample <- strsplit(c,"_")[[1]][7]
-  colsample <- strsplit(strsplit(c,"_")[[1]][9],".")[[1]][1]
+  cross_validation_results <- read.csv(paste0("./cross_validation/",c), stringsAsFactors = FALSE)
+  depth <- strsplit(c, "_")[[1]][3]
+  eta <- strsplit(c, "_")[[1]][5]
+  subsample <- strsplit(c, "_")[[1]][7]
+  colsample <- strsplit(strsplit(c, "_")[[1]][9], ".")[[1]][1]
   nrounds <- nrow(cross_validation_results) - 30
   error_rate <- min(cross_validation_results$test.merror.mean)
-  cross_valitation_aggregated_results[i,] <- c(depth,eta,subsample,colsample,nrounds,error_rate)
+  cross_valitation_aggregated_results[i,] <- c(depth, eta, subsample, colsample, nrounds, error_rate)
 }
 
 write.csv(cross_valitation_aggregated_results, file = "cross_valitation_aggregated_results.csv", row.names = FALSE)
