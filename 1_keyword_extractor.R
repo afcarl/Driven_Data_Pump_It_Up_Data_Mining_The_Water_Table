141a73b27fbd71343156e1bba857ee11d3660859
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2016-09-04 09:21:04 +0200

    Update 1_keyword_extractor.R

--- 1_keyword_extractor.R
+++ 1_keyword_extractor.R
@@ -1,17 +1,13 @@
-#####
-#
-# The train dataset has a large number of string variables that are evenly distributed.
-#
-#####
+#---------------------------------------------------------------------------------------------
+# The train dataset has a large number of string variables that are not uniformly distributed.
+#---------------------------------------------------------------------------------------------
 
 train <- read.csv("./raw_dataset/train.csv", stringsAsFactors = FALSE)
 
-#####
-#
+#------------------------------------------------------------------------------------------------
 # The 100 most common values of string variables are extracted.
 # If the variable has fewer unique values than 100 the remaining elements of the vector are NAs.
-# 
-#####
+#------------------------------------------------------------------------------------------------
 
 unique_values_1 <- sort(table(train$installer), decreasing = TRUE)[1:100]
 unique_values_2 <- sort(table(train$lga), decreasing = TRUE)[1:100]
@@ -25,11 +21,9 @@ unique_values_9 <- sort(table(train$region), decreasing = TRUE)[1:100]
 unique_values_10 <- sort(table(train$scheme_management), decreasing = TRUE)[1:100]
 unique_values_11 <- sort(table(train$extraction_type), decreasing = TRUE)[1:100]
 
-#####
-#
+#-----------------------------------------------
 # The vectors are concatenated into a dataframe.
-#
-#####
+#-----------------------------------------------
 
 keywords <- data.frame(names(unique_values_1),
                        names(unique_values_2),
@@ -44,11 +38,9 @@ keywords <- data.frame(names(unique_values_1),
                        names(unique_values_11),                       
                        stringsAsFactors = FALSE)
 
-#####
-#
+#---------------------------------------------------
 # The columns are named after the original variable.
-#
-#####
+#---------------------------------------------------
 
 colnames(keywords) <- c("installer",
                         "lga",
@@ -62,10 +54,8 @@ colnames(keywords) <- c("installer",
                         "scheme_management",
                         "extraction_type")
 
-#####
-#
+#-------------------------------------------------------
 # The dataframe is saved for the data cleaning process.
-#
-#####
+#-------------------------------------------------------
 
 write.csv(keywords, "./clean_dataset/keywords.csv", row.names = FALSE)
