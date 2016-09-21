e09eb866ba0321d7beaf076105e9999f5b9118d9
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2016-09-21 21:20:15 +0100

    Update 4_cross_validation_result_aggregation.R

--- 4_cross_validation_result_aggregation.R
+++ 4_cross_validation_result_aggregation.R
@@ -1,9 +1,7 @@
-#####
-#
+#---------------------------------------------------------------------------------
 # The cross validation results are in different comma separated values files.
 # The results can be aggregated into a data frame, where results can be compared.
-#
-#####
+#---------------------------------------------------------------------------------
 
 cross_validation <- list.files("./cross_validation/")
 cross_valitation_aggregated_results <- data.frame(matrix(0, length(cross_validation), 6))
@@ -14,16 +12,15 @@ colnames(cross_valitation_aggregated_results) <- c("depth",
                                                    "nrounds",
                                                    "error_rate")
                                                    
-#####
-#
+
+#---------------------------------------------------------------------------------------------------------
 # The table contains the number of trees, depth, eta, the column and row sample ratio and the test error.
-#
-#####
+#---------------------------------------------------------------------------------------------------------
 
 i <- 0
 for (c in cross_validation){
   i <- i + 1
-  cross_validation_results <- read.csv(paste0("./cross_validation/",c), stringsAsFactors = FALSE)
+  cross_validation_results <- read.csv(paste0("./cross_validation/", c), stringsAsFactors = FALSE)
   depth <- strsplit(c, "_")[[1]][3]
   eta <- strsplit(c, "_")[[1]][5]
   subsample <- strsplit(c, "_")[[1]][7]
